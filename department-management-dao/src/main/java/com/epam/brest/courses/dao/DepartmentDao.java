d8d2efd7dd012fea3fd4f821b548f158970e1494
Alexander Ignathick <alexanderignathick@gmail.com>
2020-02-13 23:52:17 +0300

    make dao and model layers dependencyes, using maven pom config files

---
+++ department-management-dao/src/main/java/com/epam/brest/courses/dao/DepartmentDao.java
@@ -0,0 +1,9 @@
+package com.epam.brest.courses.dao;
+
+import com.epam.courses.model.Department;
+
+import java.util.List;
+
+public interface DepartmentDao {
+    List<Department> getDeoartments();
+}
